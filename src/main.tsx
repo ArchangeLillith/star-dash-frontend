e8aff46df7eca1263307c454622d5b17207b2054
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-08-30 11:31:08 -0400

    deleted un-needed import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import "./styles/sass/index.scss";
 import NavBar from "./layout/navbar/Navbar.tsx";
 
