4087bfbe441f1502f288816acaf84762e1893534
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-11-22 16:50:27 -0500

    Transition on most pages, small organizational changes to file structure

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,22 +1,17 @@
 import { useRoutes } from "react-router-dom";
 import HomePage from "../features/home-page/HomePage";
-import JoinRun from "../features/join-run/JoinRun";
-import RunData from "../features/run-data/RunData";
 import Schedule from "../features/schedule/Schedule";
-import Login from "../features/login-register/Login";
-import Register from "../features/login-register/Register";
+import Login from "../features/login/Login";
+import Register from "../features/register/Register";
 import CreateRun from "../features/create-run/CreateRun";
 import JoinMarathon from "../features/join-run/marathon/JoinMarathon";
 import JoinCarnival from "../features/join-run/cheerful-carnival/JoinCarnival";
-import AuthHelp from "../features/help-page/auth-help/AuthHelp";
-import PublicHelp from "../features/help-page/public-help/PublicHelp";
+import HelpPage from "../features/help-page/Help";
 
 const App = () => {
 	const routes = useRoutes([
 		{ path: "/", element: <HomePage /> },
-		{ path: "/help", element: <PublicHelp /> },
-		{ path: "/auth-help", element: <AuthHelp /> },
-		{ path: "/data", element: <RunData /> },
+		{ path: "/help", element: <HelpPage /> },
 		{ path: "/schedule", element: <Schedule /> },
 		{ path: "/event", element: <Schedule /> },
 		{ path: "/login", element: <Login /> },
