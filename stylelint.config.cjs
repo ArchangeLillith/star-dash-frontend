4cadf7fa20d747923a81beafde837412f55db854
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-11-25 13:31:15 -0500

    SCSS fixed after linter broke it

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-scss'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
   rules: {
-    'block-no-empty': null,
-    'color-no-invalid-hex': true,
-    'declaration-colon-space-after': 'always',
-    indentation: 2,
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-pattern': '^[-_a-z]+$',
+    'scss/partial-no-import': null, // Allow partials with no direct imports
   },
 };
